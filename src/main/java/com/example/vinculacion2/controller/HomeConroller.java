ca52f3cf897d82a3690f3533d1ea6b965308c0d3
eguarangao <69706207+eguarangao@users.noreply.github.com>
2023-03-01 20:58:50 -0500

    Inicio

---
+++ src/main/java/com/example/vinculacion2/controller/HomeConroller.java
@@ -0,0 +1,36 @@
+package com.example.vinculacion2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeConroller {
+    @GetMapping({"/", "/home", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/powerpoint")
+    public String powerpoint() {
+        return "powerpoint";
+    }
+
+    @GetMapping("/word")
+    public String word() {
+        return "word";
+    }
+
+    @GetMapping("/pdf")
+    public String pdf() {
+        return "pdf";
+    }
+
+    @GetMapping("/audiolibros")
+    public String audiolibros() {
+        return "audiolibros";
+    }
+    @GetMapping("/nvda")
+    public String nvda() {
+        return "nvda";
+    }
+}
