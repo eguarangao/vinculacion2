482ff7e1d4063277653771b0b21158c8e68aa14f
Jonathan Cevallos <Jonathancevv@gmail.com>
2023-03-03 07:19:20 -0500

    se impleto la funcionalidad de cambair efectos de texto de pagina

--- src/main/resources/static/js/ColorTexto.js
+++ src/main/resources/static/js/ColorTexto.js
@@ -1,9 +1,9 @@
-
 function cambiarColorTextoAzul() {
     var elementosDeTexto = document.getElementsByTagName("body")[0].getElementsByTagName("*");
     for (var i = 0; i < elementosDeTexto.length; i++) {
         elementosDeTexto[i].style.color = "#005b8f";
     }
+    document.body.style.backgroundColor = "white";
 }
 
 function cambiarColorTextoVerde() {
@@ -11,6 +11,7 @@ function cambiarColorTextoVerde() {
     for (var i = 0; i < elementosDeTexto.length; i++) {
         elementosDeTexto[i].style.color = "green";
     }
+    document.body.style.backgroundColor = "white";
 }
 
 function cambiarColorTextoNegro() {
@@ -18,11 +19,68 @@ function cambiarColorTextoNegro() {
     for (var i = 0; i < elementosDeTexto.length; i++) {
         elementosDeTexto[i].style.color = "black";
     }
+    document.body.style.backgroundColor = "white";
 }
 
 function cambiarColorTextoBlanco() {
     var elementosDeTexto = document.getElementsByTagName("body")[0].getElementsByTagName("*");
     for (var i = 0; i < elementosDeTexto.length; i++) {
         elementosDeTexto[i].style.color = "white";
+    }
+    document.body.style.backgroundColor = "black";
+}
+
+function cambiarFuenteDislexia() {
+    // Cambiar la fuente de todos los elementos de la página
+    var elementosDeTexto = document.getElementsByTagName("body")[0].getElementsByTagName("*");
+    for (var i = 0; i < elementosDeTexto.length; i++) {
+        elementosDeTexto[i].style.fontFamily = "OpenDyslexic, Arial, sans-serif";
+    }
+
+    // Ajustar el tamaño de fuente para mejorar la legibilidad
+    document.body.style.fontSize = "1.2em";
+}
+
+
+function aumentarTamañoFuente() {
+    // Obtener el elemento body
+    var body = document.getElementsByTagName('body')[0];
+
+    // Obtener el tamaño actual de fuente
+    var currentFontSize = parseFloat(window.getComputedStyle(body, null).getPropertyValue('font-size'));
+
+    // Aumentar el tamaño de fuente en 1 píxel
+    var newFontSize = currentFontSize + 1;
+
+    // Establecer el nuevo tamaño de fuente en el elemento body
+    body.style.fontSize = newFontSize + 'px';
+}
+
+
+function disminuirTamanoFuente() {
+    // Obtener el elemento body
+    var body = document.getElementsByTagName('body')[0];
+
+    // Obtener el tamaño actual de fuente
+    var currentFontSize = parseFloat(window.getComputedStyle(body, null).getPropertyValue('font-size'));
+
+    // Calcular el nuevo tamaño de fuente
+    var newFontSize = currentFontSize - 1;
+
+    // Establecer el nuevo tamaño de fuente en el elemento body
+    body.style.fontSize = newFontSize + 'px';
+}
+
+
+function restablecerEstiloTexto() {
+    // Obtener todos los elementos de la página
+    var elementos = document.getElementsByTagName('*');
+
+    // Establecer el tipo de letra, tamaño de fuente y peso de fuente para cada elemento
+    for (var i = 0; i < elementos.length; i++) {
+        elementos[i].style.fontFamily = "Nunito";
+        elementos[i].style.fontSize = "16px";
+        elementos[i].style.fontWeight = "400";
+        elementos[i].style.lineHeight = "24px";
     }
 }
