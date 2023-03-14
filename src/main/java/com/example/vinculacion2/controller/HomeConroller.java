d7b14c5ede0b6bd2c2474fb047dbbf38901544b7
ESGO <ebertguaranga@gmail.com>
2023-03-14 17:19:47 -0500

    cambio del index por accesibilidad

--- src/main/java/com/example/vinculacion2/controller/HomeConroller.java
+++ src/main/java/com/example/vinculacion2/controller/HomeConroller.java
@@ -1,14 +1,13 @@
 package com.example.vinculacion2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeConroller {
-    @GetMapping({"/", "/home", "/index"})
-    public String index() {
-        return "index";
+    @GetMapping({"/", "/home", "/accesibilidad"})
+    public String accesibilidad() {
+        return "accesibilidad";
     }
 
     @GetMapping("/powerpoint")
